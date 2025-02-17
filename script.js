79b87bd2cd4c6690b0ecb6e6223e8b7934419873
FurkanIpek0 <170216383+FurkanIpek0@users.noreply.github.com>
2025-02-17 16:07:58 +0300

    refactor: solid code and fixes

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
+
+//drag
 const boxDrag = document.querySelector('#box-drag');
-const article = document.querySelector('#article');
-const rect = article.getBoundingClientRect();
-
-function onMouseMove(event) {
-    article.style.width = `${event.clientX - rect.left}px`;
-    console.log(article.clientX);
-}
-
-function onMouseUp() {
-    document.removeEventListener('mousemove', onMouseMove);
-    document.removeEventListener('mouseup', onMouseUp);
-}
 
 boxDrag.addEventListener('mousedown', (event) => {
+    const article = event.target.previousElementSibling;
+    const rect = article.getBoundingClientRect();
+    
+    const onMouseMove = (event) => {
+        article.style.width = `${event.clientX - rect.left}px`;
+    }
+    
+    const onMouseUp = () => {
+        document.removeEventListener('mousemove', onMouseMove);
+        document.removeEventListener('mouseup', onMouseUp);
+    }
+    
     document.addEventListener('mousemove', onMouseMove);
     document.addEventListener('mouseup', onMouseUp);
 });
+
